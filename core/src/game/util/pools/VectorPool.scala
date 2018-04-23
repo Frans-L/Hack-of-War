556d41ae3479d152b625140ad62425dc96bd0eec
Frans-L <jonkun.muun@hotmail.com>
2018-04-23 13:40:36 +0300

    Refractored pooling

---
+++ core/src/game/util/pools/VectorPool.scala
@@ -0,0 +1,26 @@
+package game.util
+
+import com.badlogic.gdx.math.Vector2
+import com.badlogic.gdx.utils.{Pool, Pools}
+
+
+/**
+  * Wrapper to a normal pool to make vectors to support pooling.
+  * Vectors wouldn't normally be reset since they doesn't extend Poolable.
+  */
+object VectorPool {
+
+  //pools vectors to reduce garbage collector
+  //Pools.get returns same pool, if same kind pool already exists
+  private lazy val pool: Pool[Vector2] = Pools.get(classOf[Vector2])
+
+  def obtain(): Vector2 = pool.obtain().set(0, 0) //have to reset here
+
+  def obtain(v: Vector2): Vector2 = pool.obtain().set(v)
+
+  def obtain(x: Float, y: Float): Vector2 = pool.obtain().set(x, y)
+
+  def free(v: Vector2): Unit = pool.free(v)
+
+  def clear(): Unit = pool.clear()
+}
